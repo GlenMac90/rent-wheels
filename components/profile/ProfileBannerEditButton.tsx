3979416291ce162b8fc72606bf8e4755baef0743
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-02-16 23:28:09 +0200

    add dropzone to banner image edit

--- components/profile/ProfileBannerEditButton.tsx
+++ components/profile/ProfileBannerEditButton.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
+
 import ModalBackground from "../ModalBackground";
 import CloseButton from "../CloseButton";
 
 const ProfileBannerEditButton = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const [showEditModal, setShowEditModal] = useState(false);
 
   const handleClick = () => {
@@ -13,6 +17,15 @@ const ProfileBannerEditButton = () => {
 
   const handleInnerClick = (e: MouseEvent) => {
     e.stopPropagation();
+  };
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
   };
 
   return (
@@ -41,10 +54,32 @@ const ProfileBannerEditButton = () => {
               </div>
               <CloseButton handleClose={handleClick} />
             </div>
-            <div className="flex w-full flex-col gap-2">
+            <div className="flex w-full flex-col gap-5">
               <label className="semibold-14 md:semibold-16 text-gray-900_white">
                 Upload Image
               </label>
+              <div
+                {...getRootProps()}
+                className={`flex-center h-44 w-full flex-col rounded-ten border border-dashed border-gray-400 ${isDragActive && "bg-white-200_gray-800"}`}
+              >
+                <input
+                  {...getInputProps()}
+                  className="hidden"
+                  ref={fileInputRef}
+                />
+                <button type="button" onClick={handleButtonClick}>
+                  <FiUpload className="text-2xl text-blue-500" />
+                </button>
+                <p className="medium-14 text-gray-blue-100 mt-4">
+                  Drag and drop an image, or{" "}
+                  <span className="text-blue-500" onClick={handleButtonClick}>
+                    Browse
+                  </span>
+                </p>
+                <span className="base-14 text-gray-400_white-100 mt-2">
+                  High resolution images (png, jpg, gif)
+                </span>
+              </div>
             </div>
           </div>
         </ModalBackground>
