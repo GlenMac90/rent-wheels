e4a7e846d13e74554c376daf9dd95fae0c9e4813
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-04-09 20:16:42 +0300

    start work on transaction page UI

--- app/(root)/transaction/[id]/page.tsx
+++ app/(root)/transaction/[id]/page.tsx
@@ -9,6 +9,8 @@ const Transaction = async ({ params }: { params: { id: string } }) => {
     redirect("/");
   }
 
+  console.log(transaction);
+
   return (
     <main className="page-styles">
       <div className="flex-center">
